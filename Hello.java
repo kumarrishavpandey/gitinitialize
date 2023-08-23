df46f9e97af029fc7839bad4d7c47211c828d725
kumarrishavpandey <95020043+kumarrishavpandey@users.noreply.github.com>
2023-08-23 20:12:31 +0530

    first initialization via command prompt

---
+++ Hello.java
@@ -0,0 +1,12 @@
+/**
+ * Hello
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 40;
+        int z = x+y;
+        System.out.println(z);
+    }
+}
